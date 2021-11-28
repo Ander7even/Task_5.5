7c9cb2d34a4ed689cc0e5791761711f46c4cebb3
ander7even <ander7even@mail.ru>
2021-11-28 19:09:15 +0900

    Добавьте файлы проекта.

---
+++ Task_5.5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < N; j++)
+                {
+                    array[i, j] = 1 - (i + j) % 2;
+                    Console.Write("{0,4} ", array[i, j]);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
